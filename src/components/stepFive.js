a18454a609ac1d6a25c4da59e2b849f2bc7223a0
Nicholas Lange <Nicholas Lange>
2017-06-30 11:22:41 -0600

    added if statement for loading

--- src/components/stepFive.js
+++ src/components/stepFive.js
@@ -1,36 +1,54 @@
 import React, { Component } from 'react';
-import 'whatwg-fetch';
 import $ from 'jquery';
 
 class StepFive extends Component{
   constructor(props) {
   		super(props)
   		this.state = {
-  			ingredients: []
+  			ingredients: false
 
       }
-}
-  	ingredientsGrab() {
-  		var me = this;
-  		$.ajax({
-  			method: 'GET',
-  			url: 'http://localhost:3002/api/ingredients',
-  		})
-  		.then(function(ingredients){
-            me.setState({ingredients: ingredients})
-  		})
-  	}
-    componentWillMount() {
-        this.ingredientsGrab()
+  }
+    componentDidMount() {
+      var self = this;
+      $.ajax({
+        method: 'GET',
+        url: 'http://localhost:3002/api/ingredients',
+      })
+      .done(function(ingredients){
+            self.setState({ingredients: ingredients})
+      })
     }
 
 render() {
-  console.log(this.state.ingredients[0], 'in render1')
+  console.log(this.state.ingredients, 'in render')
+  if(this.state.ingredients === false){
+    return(<div><span>loading</span></div>)
+  } else {
       return(
         <div className='stepFiveBox'>
-          <span>bubbly</span>
+          <h1>{this.state.ingredients[0].name}</h1>
+          <ul>
+            <li>Hardness={this.state.ingredients[0].hardness}</li>
+            <li>Cleansing={this.state.ingredients[0].cleansing}</li>
+            <li>Condidtion={this.state.ingredients[0].condition1}</li>
+            <li>Bubbly={this.state.ingredients[0].bubbly}</li>
+            <li>Creamy={this.state.ingredients[0].creamy}</li>
+            <li>Iodine={this.state.ingredients[0].iodine}</li>
+            <li>INS={this.state.ingredients[0].ins}</li>
+            <li>Lauric={this.state.ingredients[0].lauric}</li>
+            <li>Myristic={this.state.ingredients[0].myristic}</li>
+            <li>Palmitic={this.state.ingredients[0].palmitic}</li>
+            <li>Stearic={this.state.ingredients[0].stearic}</li>
+            <li>Ricinoleic={this.state.ingredients[0].rincinoleic}</li>
+            <li>Oleic={this.state.ingredients[0].oleic}</li>
+            <li>Linoleic={this.state.ingredients[0].linoleic}</li>
+            <li>Linolenic={this.state.ingredients[0].linoleic}</li>
+            <li>NaOH SAP={this.state.ingredients[0].sap}</li>
+          </ul>
         </div>
       )
+    }
   }
 }
 
