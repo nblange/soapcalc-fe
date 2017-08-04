1c01cca2b17164b133f3841bf53bbd07d151fc8f
Nick <nicklange92@gmail.com>
2017-08-04 08:46:18 -0600

    added remove button for step 6, also made inputs for values of ingredients

--- src/components/stepFive.js
+++ src/components/stepFive.js
@@ -36,28 +36,68 @@ class StepFive extends Component{
     handleChange = (e) => {
        this.setState({counter: e});
     }
-    addOilOne = (e) => {
+    addOil = (e) => {
       if(this.state.oilNameGrabbedOne === ""){
       this.setState({oilNameGrabbedOne: this.state.ingredients[e].name})
-    } else if(this.state.oilNameGrabbedTwo === ""){
+      } else if(this.state.oilNameGrabbedTwo === ""){
       this.setState({oilNameGrabbedTwo: this.state.ingredients[e].name})
-    } else if(this.state.oilNameGrabbedThree === ""){
+      } else if(this.state.oilNameGrabbedThree === ""){
       this.setState({oilNameGrabbedThree: this.state.ingredients[e].name})
-    } else if(this.state.oilNameGrabbedFour === ""){
+      } else if(this.state.oilNameGrabbedFour === ""){
       this.setState({oilNameGrabbedFour: this.state.ingredients[e].name})
-    } else if(this.state.oilNameGrabbedFive === ""){
+      } else if(this.state.oilNameGrabbedFive === ""){
       this.setState({oilNameGrabbedFive: this.state.ingredients[e].name})
-    } else if(this.state.oilNameGrabbedSix === ""){
+      } else if(this.state.oilNameGrabbedSix === ""){
       this.setState({oilNameGrabbedSix: this.state.ingredients[e].name})
-    } else if(this.state.oilNameGrabbedSeven === ""){
+      } else if(this.state.oilNameGrabbedSeven === ""){
       this.setState({oilNameGrabbedSeven: this.state.ingredients[e].name})
-    } else if(this.state.oilNameGrabbedEight === ""){
+      } else if(this.state.oilNameGrabbedEight === ""){
       this.setState({oilNameGrabbedEight: this.state.ingredients[e].name})
-    } else if(this.state.oilNameGrabbedNine === ""){
+      } else if(this.state.oilNameGrabbedNine === ""){
       this.setState({oilNameGrabbedNine: this.state.ingredients[e].name})
-    } else if(this.state.oilNameGrabbedTen === ""){
+      } else if(this.state.oilNameGrabbedTen === ""){
       this.setState({oilNameGrabbedTen: this.state.ingredients[e].name})
+      }
     }
+
+    removeOilOne(){
+      this.setState({oilNameGrabbedOne: ""})
+    }
+
+    removeOilTwo(){
+      this.setState({oilNameGrabbedTwo: ""})
+    }
+
+    removeOilThree(){
+      this.setState({oilNameGrabbedThree: ""})
+    }
+
+    removeOilFour(){
+      this.setState({oilNameGrabbedFour: ""})
+    }
+
+    removeOilFive(){
+      this.setState({oilNameGrabbedFive: ""})
+    }
+
+    removeOilSix(){
+      this.setState({oilNameGrabbedSix: ""})
+    }
+
+    removeOilSeven(){
+      this.setState({oilNameGrabbedSeven: ""})
+    }
+
+    removeOilEight(){
+      this.setState({oilNameGrabbedEight: ""})
+    }
+
+    removeOilNine(){
+      this.setState({oilNameGrabbedNine: ""})
+    }
+
+    removeOilTen(){
+      this.setState({oilNameGrabbedTen: ""})
     }
 
 
@@ -69,7 +109,7 @@ class StepFive extends Component{
           {self.state.ingredients.map(function(ingredients, i){
             return <div>
               <a key={i} href="#" onClick={() => self.handleChange(i)}>{ingredients.name}</a>
-              <a key={self.state.ingredients.id} href='#' onClick={() => self.addOilOne(i)}>+</a>
+              <a key={self.state.ingredients.id} href='#' onClick={() => self.addOil(i)}>+</a>
             </div>
           })}
         </div>
@@ -85,65 +125,65 @@ render() {
         <div className='stepFiveAndSixBox'>
           <div className='stepFive'>
             <span className='stepFiveList'>{this.listAll()}</span>
-            <ul className='stepFiveValues'>
-              <li>Hardness=<span className="indentStyleHardness">{this.state.ingredients[this.state.counter].hardness}</span></li>
-              <li>Cleansing=<span className="indentStyleCleansing">{this.state.ingredients[this.state.counter].cleansing}</span></li>
-              <li>Condidtion=<span className="indentStyleCondition">{this.state.ingredients[this.state.counter].condition1}</span></li>
-              <li>Bubbly=<span className="indentStyleBubbly">{this.state.ingredients[this.state.counter].bubbly}</span></li>
-              <li>Creamy=<span className="indentStyleCreamy">{this.state.ingredients[this.state.counter].creamy}</span></li>
-              <li>Iodine=<span className="indentStyleIodine">{this.state.ingredients[this.state.counter].iodine}</span></li>
-              <li>INS=<span className="indentStyleIns">{this.state.ingredients[this.state.counter].ins}</span></li>
-              <li>Lauric=<span className="indentStyleLauric">{this.state.ingredients[this.state.counter].lauric}</span></li>
-              <li>Myristic=<span className="indentStyleMyristic">{this.state.ingredients[this.state.counter].myristic}</span></li>
-              <li>Palmitic=<span className="indentStylePalmitic">{this.state.ingredients[this.state.counter].palmitic}</span></li>
-              <li>Stearic=<span className="indentStyleStearic">{this.state.ingredients[this.state.counter].stearic}</span></li>
-              <li>Ricinoleic=<span className="indentStyleRincinoleic">{this.state.ingredients[this.state.counter].rincinoleic}</span></li>
-              <li>Oleic=<span className="indentStyleOleic">{this.state.ingredients[this.state.counter].oleic}</span></li>
-              <li>Linoleic=<span className="indentStyleLinoleic">{this.state.ingredients[this.state.counter].linoleic}</span></li>
-              <li>Linolenic=<span className="indentStyleLinolenic">{this.state.ingredients[this.state.counter].linolenic}</span></li>
-              <li>NaOH SAP=<span className="indentStyleSap">{this.state.ingredients[this.state.counter].sap}</span></li>
-            </ul>
+            <form className='stepFiveValues'>
+              <label>Hardness=<input className="indentStyleHardness" value={this.state.ingredients[this.state.counter].hardness}/></label><br/>
+              <label>Cleansing=<input className="indentStyleCleansing" value={this.state.ingredients[this.state.counter].cleansing}/></label><br/>
+              <label>Condidtion=<input className="indentStyleCondition" value={this.state.ingredients[this.state.counter].condition1}/></label><br/>
+              <label>Bubbly=<input className="indentStyleBubbly" value={this.state.ingredients[this.state.counter].bubbly}/></label><br/>
+              <label>Creamy=<input className="indentStyleCreamy" value={this.state.ingredients[this.state.counter].creamy}/></label><br/>
+              <label>Iodine=<input className="indentStyleIodine" value={this.state.ingredients[this.state.counter].iodine}/></label><br/>
+              <label>INS=<input className="indentStyleIns" value={this.state.ingredients[this.state.counter].ins}/></label><br/>
+              <label>Lauric=<input className="indentStyleLauric" value={this.state.ingredients[this.state.counter].lauric}/></label><br/>
+              <label>Myristic=<input className="indentStyleMyristic" value={this.state.ingredients[this.state.counter].myristic}/></label><br/>
+              <label>Palmitic=<input className="indentStylePalmitic" value={this.state.ingredients[this.state.counter].palmitic}/></label><br/>
+              <label>Stearic=<input className="indentStyleStearic" value={this.state.ingredients[this.state.counter].stearic}/></label><br/>
+              <label>Ricinoleic=<input className="indentStyleRincinoleic" value={this.state.ingredients[this.state.counter].rincinoleic}/></label><br/>
+              <label>Oleic=<input className="indentStyleOleic" value={this.state.ingredients[this.state.counter].oleic}/></label><br/>
+              <label>Linoleic=<input className="indentStyleLinoleic" value={this.state.ingredients[this.state.counter].linoleic}/></label><br/>
+              <label>Linolenic=<input className="indentStyleLinolenic" value={this.state.ingredients[this.state.counter].linolenic}/></label><br/>
+              <label>NaOH SAP=<input className="indentStyleSap" value={this.state.ingredients[this.state.counter].sap}/></label><br/>
+            </form>
           </div>
           <div className='stepSixBox'>
             <h1 className="stepSixHeading"> Recipe Oil List </h1>
               <form className="stepSixForm">
-                <label> 1
+                <label> 1 <a classname="removeOilButton" href= "#" onClick={() => this.removeOilOne()}>-</a>
                   <input className="inputOne" type="text" value={this.state.oilNameGrabbedOne}/>
                 </label>
                 <br/>
-                <label> 2
+                <label> 2 <a classname="removeOilButton" href= "#" onClick={() => this.removeOilTwo()}>-</a>
                   <input className="inputTwo" type="text" value={this.state.oilNameGrabbedTwo}/>
                 </label>
                 <br/>
-                <label> 3
+                <label> 3 <a classname="removeOilButton" href= "#" onClick={() => this.removeOilThree()}>-</a>
                   <input className="inputThree" type="text" value={this.state.oilNameGrabbedThree}/>
                 </label>
                 <br/>
-                <label> 4
+                <label> 4 <a classname="removeOilButton" href= "#" onClick={() => this.removeOilFour()}>-</a>
                   <input className="inputFour" type="text" value={this.state.oilNameGrabbedFour}/>
                 </label>
                 <br/>
-                <label> 5
+                <label> 5 <a classname="removeOilButton" href= "#" onClick={() => this.removeOilFive()}>-</a>
                   <input className="inputFive" type="text" value={this.state.oilNameGrabbedFive}/>
                 </label>
                 <br/>
-                <label> 6
+                <label> 6 <a classname="removeOilButton" href= "#" onClick={() => this.removeOilSix()}>-</a>
                   <input className="inputSix" type="text" value={this.state.oilNameGrabbedSix}/>
                 </label>
                 <br/>
-                <label> 7
+                <label> 7 <a classname="removeOilButton" href= "#" onClick={() => this.removeOilSeven()}>-</a>
                   <input className="inputSeven" type="text" value={this.state.oilNameGrabbedSeven}/>
                 </label>
                 <br/>
-                <label> 8
+                <label> 8 <a classname="removeOilButton" href= "#" onClick={() => this.removeOilEight()}>-</a>
                   <input className="inputEight" type="text" value={this.state.oilNameGrabbedEight}/>
                 </label>
                 <br/>
-                <label> 9
+                <label> 9 <a classname="removeOilButton" href= "#" onClick={() => this.removeOilNine()}>-</a>
                   <input className="inputNine" type="text" value={this.state.oilNameGrabbedNine}/>
                 </label>
                 <br/>
-                <label> 10
+                <label> 10 <a classname="removeOilButton" href= "#" onClick={() => this.removeOilTen()}>-</a>
                   <input className="inputTen" type="text" value={this.state.oilNameGrabbedTen}/>
                 </label>
               </form>
